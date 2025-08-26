a0a74fefce58bfaae19001301fd08fa37b6b97f4
Sumit Kumar <sharansumitkumar@gmail.com>
2025-08-26 22:04:36 +0530

    Made MetaDataFinder private

--- lib/phone_numbers_parser.dart
+++ lib/phone_numbers_parser.dart
@@ -8,7 +8,6 @@ export 'src/phone_number.dart';
 export 'src/validation/phone_number_type.dart';
 export 'src/parsers/phone_number_exceptions.dart';
 export 'src/iso_codes/iso_code.dart';
-export 'src/metadata/metadata_finder.dart';
 // to remove next major:
 export 'src/range/phone_number_range.dart';
 export 'src/parsers/phone_parser.dart';
